36d696c6d3cc503ebe0b373a4fc06fb2d695f98c
Jiajie Chen <c@jia.je>
2025-12-07 15:36:49 +0800

    Try to fix ci by adding --std=c++17

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CFLAGS="--cuda-gpu-arch=sm_70 \
     -L/usr/local/cuda/lib64 \
-    -lcudart_static -ldl -lrt -pthread --save-temps -v"
+    -lcudart_static -ldl -lrt -pthread --std=c++17 --save-temps -v"
 CXX="${CXX:-clang++}"
 
 $CXX axpy.cu -o axpy $CFLAGS
